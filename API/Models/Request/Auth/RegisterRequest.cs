07f561efa7c12a4177d2648811dfc4dd4495f11a
danielNG <dacnguyen9101@gmail.com>
2023-07-09 22:16:44 +0700

    feat: get profile

--- API/Models/Request/Auth/RegisterRequest.cs
+++ API/Models/Request/Auth/RegisterRequest.cs
@@ -16,6 +16,6 @@ public class RegisterRequest : IMapTo<User>
     [Required, MinLength(3), MaxLength(32)]
     public string ReEnteredPassword { get; set; }
 
-    [Required]
+    [Required, Range(1, 4)]
     public Role Role { get; set; }
 }
